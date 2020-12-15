e258b4e8b8f1e59e374c2d2e5f7bbb3637688ad0
ninest <parth.kabra@gmail.com>
2020-12-15 12:54:25 +0800

    Add image sharing

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,11 @@
-import React, { useState } from "react";
 import Clock from "./components/Clock";
+import Menu from "./components/Menu";
 
 function App() {
-  const [showMenu, setShowMenu] = useState(false);
-
   return (
-    // Close menu (if open) when anything clicked
-    <main onClick={() => (showMenu ? setShowMenu(false) : null)}>
+    <main>
       <Clock />
-      <div className="three-dot">
-        <button
-          className="three-dot-button button button-icon"
-          onClick={() => setShowMenu(!showMenu)}
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512">
-            <path d="M96 184c39.8 0 72 32.2 72 72s-32.2 72-72 72-72-32.2-72-72 32.2-72 72-72zM24 80c0 39.8 32.2 72 72 72s72-32.2 72-72S135.8 8 96 8 24 40.2 24 80zm0 352c0 39.8 32.2 72 72 72s72-32.2 72-72-32.2-72-72-72-72 32.2-72 72z" />
-          </svg>
-        </button>
-        {showMenu && (
-          <div className="menu">
-            <div className="menu-item" tabIndex={1}>
-              Share
-            </div>
-
-            <a
-              tabIndex={1}
-              href="https://github.com/ninest/hexy-time"
-              target="_blank"
-              rel="noreferrer"
-              className="menu-item"
-            >
-              GitHub
-            </a>
-          </div>
-        )}
-      </div>
+      <Menu />
     </main>
   );
 }
---
+++ src/components/Menu.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { shareImage } from "./share";
+
+function Menu() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  async function share() {
+    setShowMenu(false);
+    await shareImage();
+  }
+
+  return (
+    <div className="three-dot">
+      <button
+        className="three-dot-button button button-icon"
+        onClick={() => setShowMenu(!showMenu)}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512">
+          <path d="M96 184c39.8 0 72 32.2 72 72s-32.2 72-72 72-72-32.2-72-72 32.2-72 72-72zM24 80c0 39.8 32.2 72 72 72s72-32.2 72-72S135.8 8 96 8 24 40.2 24 80zm0 352c0 39.8 32.2 72 72 72s72-32.2 72-72-32.2-72-72-72-72 32.2-72 72z" />
+        </svg>
+      </button>
+
+      {showMenu && (
+        <div className="menu">
+          <div
+            onClick={() => share()}
+            className="menu-item"
+            tabIndex={1}
+            role="button"
+          >
+            Share
+          </div>
+
+          <a
+            tabIndex={1}
+            href="https://github.com/ninest/hexy-time"
+            target="_blank"
+            rel="noreferrer"
+            className="menu-item"
+            role="button"
+          >
+            GitHub
+          </a>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Menu;
