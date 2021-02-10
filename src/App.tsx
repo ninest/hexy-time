a22d85d1e6351924302adfce6c020510cd2f7211
ninest <parth.kabra@gmail.com>
2021-02-10 13:12:30 +0800

    Add wakelock

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react";
 import Clock from "./components/Clock";
 import Menu from "./components/Menu";
 
 function App() {
+  const [textGlow, setTextGlow] = useState(false);
+
+  /* Request wake lock */
+  useEffect(() => {
+    if ("wakeLock" in navigator) {
+      // @ts-ignore
+      navigator.wakeLock
+        .request("screen")
+        .then(() => {
+          console.log("Wake lock activated!");
+          setTextGlow(true);
+
+          setTimeout(() => {
+            setTextGlow(false);
+          }, 3500);
+        })
+        .catch(() => {
+          console.log("No wake lock");
+          setTextGlow(false);
+        });
+    }
+  }, []);
+
   return (
     <main>
-      <Clock />
+      <Clock textGlow={textGlow} />
       <Menu />
     </main>
   );
--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import getTime from "../hex-time";
 
-function Clock() {
+function Clock({ textGlow }: { textGlow: boolean }) {
   const [showHex, setShowHex] = useState<boolean>(true);
   const [time, setTime] = useState<{ normal: string; hex: string }>(
     getTime(new Date())
@@ -21,14 +21,14 @@ function Clock() {
 
   useEffect(() => {
     // style={{ backgroundColor: `#${time.hex}` }}
-    document.body.style.backgroundColor = `#${time.hex}`
+    document.body.style.backgroundColor = `#${time.hex}`;
   }, [time.hex]);
 
   return (
-    <div className="clock" >
+    <div className="clock">
       <div className="clock-time" onClick={() => setShowHex(!showHex)}>
         {showHex ? <div className="hash">#</div> : null}
-        <div className="clock-time-value">
+        <div className={`clock-time-value ${textGlow && "clock-time-glow"}`}>
           {(showHex ? time.hex : time.normal).split("").map((value, index) => {
             return (
               <div
